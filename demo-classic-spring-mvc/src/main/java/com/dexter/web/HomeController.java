4e2837fbc97a2fadf5a76bc5ca9518810068978e
Dhiman <dhiman.talapatra@gmail.com>
2023-07-01 12:03:21 +0200

    Addes Styling
    Added main CSS file and differentiated normal and error message

--- demo-classic-spring-mvc/src/main/java/com/dexter/web/HomeController.java
+++ demo-classic-spring-mvc/src/main/java/com/dexter/web/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 
 /**
@@ -31,22 +32,18 @@ public class HomeController {
  @RequestMapping(value = "/addUser", method = RequestMethod.POST)
  public String serviceTest(@ModelAttribute User user, Model model) {
 	 log.debug("Invoked serviceTest method");
-	 if(null==user) {
-		 model.addAttribute("msg","No user received");
-		 log.error("No user received");
-		 return "home";
-	 }
 	 if(null==user.getName() || user.getName().trim().isEmpty()) {
-		 model.addAttribute("msg","Enter Name");
+		 model.addAttribute("error","Enter Name");
 		 log.error("Name is blank");
 		 return "home";
 	 }
 	 if(user.getAge()<10) {
-		 model.addAttribute("msg","Age cannot be < 10");
+		 model.addAttribute("error","Age cannot be < 10");
 		 log.error("Age {} cannot be < 10",user.getAge());
 		 return "home";
 	 }
 	 log.debug("Validated model successfully {}",user.toString());
+	 model.addAttribute("user",new User());
 	 model.addAttribute("msg",String.format("Welcome %s (%s)",user.getName(),user.getAge()));
 	 return "home";
  }
